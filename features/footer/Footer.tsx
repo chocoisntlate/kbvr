10701dd0633ad6677a41f8e7464efd1c397782ad
phoomsr <130369559+chocoisntlate@users.noreply.github.com>
2026-08-02 22:09:15 +0100

    footer change

--- features/footer/Footer.tsx
+++ features/footer/Footer.tsx
@@ -3,10 +3,6 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <div className="p-4 flex items-center justify-between text-xs text-neutral-600 dark:text-neutral-400">
-      <div className="flex items-center gap-1.5">
-        <span>Interactive shortcut visualisation</span>
-      </div>
-
       <Link
         href="https://github.com/chocoisntlate/kbvr"
         target="_blank"
@@ -21,7 +17,6 @@ export default function Footer() {
         >
           <path d="M12 .5C5.65.5.5 5.65.5 12c0 5.09 3.29 9.4 7.86 10.93.57.1.79-.25.79-.55 0-.27-.01-1.17-.02-2.12-3.2.7-3.87-1.36-3.87-1.36-.53-1.34-1.29-1.7-1.29-1.7-1.05-.72.08-.7.08-.7 1.17.08 1.78 1.2 1.78 1.2 1.03 1.77 2.72 1.26 3.38.96.1-.75.4-1.26.73-1.55-2.55-.29-5.23-1.28-5.23-5.68 0-1.25.45-2.28 1.19-3.08-.12-.29-.51-1.46.11-3.05 0 0 .97-.31 3.18 1.18a11 11 0 0 1 5.79 0c2.2-1.49 3.17-1.18 3.17-1.18.63 1.59.24 2.76.12 3.05.74.8 1.19 1.83 1.19 3.08 0 4.41-2.69 5.38-5.25 5.67.41.36.78 1.07.78 2.16 0 1.56-.01 2.82-.01 3.2 0 .3.21.66.8.55A11.5 11.5 0 0 0 23.5 12C23.5 5.65 18.35.5 12 .5Z" />
         </svg>
-        GitHub
       </Link>
     </div>
   );
